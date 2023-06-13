bf45cda488153c101239ce8aa1ffb3bc942411a0
Jawdat Tayfour <tajwdat3@gmail.com>
2023-06-13 18:22:07 +0300

    Create code.py
    Problem
    You have been given a positive integer N. You need to find and print the Factorial of this number. The Factorial of a positive integer N refers to the product of all number in the range from 1 to N. You can read more about the factorial of a number here.
    
    Input Format:
    The first and only line of the input contains a single integer N denoting the number whose factorial you need to find.
    
    Output Format
    Output a single line denoting the factorial of the number N.

---
+++ code.py
@@ -0,0 +1,3 @@
+import math
+n = int(input())
+print(math.factorial(n)) # that's a built-in function in the math library in python it returns the factorial of it's int argument, I know I shouldn't be posting a 3 line worth of code on github but idk maybe a beginner will need it one day.
